e6d9f80b512cc7f11b24f37991fa3a2e7b10b145
Vasily Sovetov <vsovetov@croc.ru>
2016-09-11 22:20:29 +0300

    Update main asm->Hack file

--- projects/06/assembler/hackasm.py
+++ projects/06/assembler/hackasm.py
@@ -1,6 +1,43 @@
 #!/usr/bin/env python
+"""Main body of Hack to machine code translator"""
+
+import os
+import sys
+
+import hack_parser
+import hack_symtable
+import hack_translator
 
 
+def translate():
+	"""Main function"""
 
-if __name__ == "__main__":
-	
+	asm_file_name = sys.argv[1]
+	hack_file_name = os.path.splitext(asm_file_name)[0] + ".hack"
+
+	asm_file = open(asm_file_name, "r")
+	hack_file = open(hack_file_name, "w")
+
+	raw_asm_code = asm_file.readlines()
+	asm_code = []
+
+	symtable = hack_symtable.SymTable()
+
+	# label handling phase
+	for line in raw_asm_code:
+		(code, instruction) = hack_parser.parse_line(line)
+
+		if code is not None:
+			if code == "L":
+				symtable.add_label(instruction, len(asm_code))
+			else:
+				asm_code.append((code, instruction))
+
+	# code translation phase
+	for code, instruction in asm_code:
+		hack_file.write(hack_translator.translate(code, instruction, symtable) + "\n")
+
+	asm_file.close()
+	hack_file.close()
+
+translate()
